b5c635832f69b10fcfbc51d4b38e236cb2893198
gpopoiu <gpopoiu@bitdefender.com>
2022-02-23 18:38:18 +0200

    update

--- Lab1/Lab1/Source.cpp
+++ Lab1/Lab1/Source.cpp
@@ -1,7 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-    printf("Hello world\n");
-    printf("Test\n");
+    FILE* f = fopen("in.txt", "r");
+    if (f == NULL) {
+        printf("Cannot open\n");
+        return 0;
+    }
+    printf("Open ok\n");
+    char s[128];
+    while (!feof(f)) {
+        fscanf(f, "%s", s);
+        printf("%s\n", s);
+    }    
+    fclose(f);
+
+    printf("Introdu propozitia: ");
+    fgets(s, 128, stdin);
+    printf("%s\n", s);
+
     return 0;
 }
